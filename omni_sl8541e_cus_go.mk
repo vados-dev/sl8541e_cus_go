84d57f402ebe57898a527c2b68cb4e0bbb94294e
vados-dev <vados@vados.ru>
2025-01-16 09:57:04 +0300

    Update omni_sl8541e_cus_go.mk

--- omni_sl8541e_cus_go.mk
+++ omni_sl8541e_cus_go.mk
@@ -6,14 +6,17 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from sl8541e_cus_go device
+$(call inherit-product, device/sprd/sl8541e_cus_go/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
-
-# Inherit from sl8541e_cus_go device
-#$(call inherit-product, device/sprd/sl8541e_cus_go/device.mk)
 
 PRODUCT_DEVICE := sl8541e_cus_go
 PRODUCT_NAME := omni_sl8541e_cus_go
