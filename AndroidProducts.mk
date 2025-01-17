a3fbf56f8d2b5b4d09ed4d6e2deb1fdebfee6680
vados-dev <vados@vados.ru>
2025-01-17 21:27:29 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,8 +7,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_sl8541e_cus_go.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_sl8541e_cus_go-user \
-    omni_sl8541e_cus_go-userdebug \
-    omni_sl8541e_cus_go-eng
