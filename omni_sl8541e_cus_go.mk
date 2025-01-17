d60fc89c841cb1b04e1ee64556351522b3bb19d6
vados-dev <vados@vados.ru>
2025-01-17 13:53:35 +0300

    Update omni_sl8541e_cus_go.mk

--- omni_sl8541e_cus_go.mk
+++ omni_sl8541e_cus_go.mk
@@ -8,12 +8,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from sl8541e_cus_go device
-$(call inherit-product, device/sprd/sl8541e_cus_go/device.mk)
+#$(call inherit-product, device/sprd/sl8541e_cus_go/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := sl8541e_cus_go
 PRODUCT_NAME := omni_sl8541e_cus_go
-PRODUCT_BRAND := FX
+PRODUCT_BRAND := sprd
 PRODUCT_MODEL := D41
 PRODUCT_MANUFACTURER := sprd
 
