b52ca5cc5e0068ed23eb1db0f98cd1fed191f08c
vados-dev <vados@vados.ru>
2025-01-17 13:53:49 +0300

    Delete device.mk

--- device.mk
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-LOCAL_PATH := device/sprd/sl8541e_cus_go
