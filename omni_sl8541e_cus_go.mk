35ed51771e7ff3b67e41fba9c2f9372c0ebdbf16
vados-dev <vados@vados.ru>
2025-01-26 07:32:15 +0300

    Update omni_sl8541e_cus_go.mk

--- omni_sl8541e_cus_go.mk
+++ omni_sl8541e_cus_go.mk
@@ -20,7 +20,7 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit some common Omni stuff.
 #$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/pb-dev/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_DEVICE := sl8541e_cus_go
 PRODUCT_NAME := omni_sl8541e_cus_go
