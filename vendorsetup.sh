982babb59729812d0c9dea3b024ea4f2e6b23228
vados-dev <vados@vados.ru>
2025-02-12 11:36:22 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,4 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_sl8541e_cus_go-eng
+#add_lunch_combo omni_sl8541e_cus_go-eng
+export ANDROID_SET_JAVA_HOME=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export LC_ALL=C
+export ALLOW_MISSING_DEPENDENCIES=true
+export TARGET_COMPILE_WITH_MSM_KERNEL=false
