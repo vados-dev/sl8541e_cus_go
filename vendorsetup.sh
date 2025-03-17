40ad2ae8830c9906c58ec68bcc15bc24989dfb75
vados-dev <vados@vados.ru>
2025-03-17 15:44:54 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,8 @@
 #
 
 add_lunch_combo omni_sl8541e_cus_go-eng
+export ANDROID_SET_JAVA_HOME=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export LC_ALL=C
+export ALLOW_MISSING_DEPENDENCIES=true
+export TARGET_COMPILE_WITH_MSM_KERNEL=false
